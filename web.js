7c379e0008a3de21f955b2c20338f5c169a5c29a
Arn Sas <nanofan95@gmail.com>
2012-10-04 20:29:54 -0500

    testing

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/freed', function(req, res) {
+    response.send('MAKNOOOOOOOOOOOOOOOOOOOOOON!');
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);

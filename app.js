da92b5f84361a620ca68c8068aa65d17737000ea
Arn Sas <nanofan95@gmail.com>
2012-10-10 11:33:18 -0500

    testing processes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-
 var app = express();
 
 require('jade');
@@ -7,12 +6,21 @@ app.set('view engine', 'jade');
 app.set('view options', {layout: false});
 app.use(express.logger());
 
+var exec = require('child_process').exec, child;
+
+var text;
+
+child = exec('ls', function(error, stdout, stderr){
+  console.log('stdout: ' + stdout);
+  text = stdout;
+});
+
 app.get('/', function(req, res) {
-  res.render('index.jade', {team: "Moscow 5", losers: "TSM"}); 
+  return res.render('index.jade', {team: "Moscow 5", losers: "TSM", text: text}); 
 });
 
 app.get('/:url', function(req, res) {
-  return res.render('index.jade', {team:"Moscow 5", losers: req.params.url});
+  return res.render('index.jade', {team:"Moscow 5", losers: req.params.url, text: 'guhguh'});
 });
 
 var port = process.env.PORT || 5000;

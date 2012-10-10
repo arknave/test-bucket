c3218bc6dc752e580dfaa58b9cb2518eb97857fb
Arn Sas <nanofan95@gmail.com>
2012-10-10 15:25:36 -0500

    pythrone

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var exec = require('child_process').exec, child;
 
 var text, err;
 
-child = exec('java -cp Java/ Herro', function(error, stdout, stderr){
+child = exec('python Python/herro.py', function(error, stdout, stderr){
   console.log('stdout: ' + stdout);
   console.log('stderr: ' + stderr);
   text = stdout;

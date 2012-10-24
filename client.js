ea310a47130d9309a87f59ab699120994f41ad99
arknave <arnavsastry@gmail.com>
2012-10-24 15:39:04 -0500

    homygodhelpoursoulz

---
+++ client.js
@@ -0,0 +1,11 @@
+alert("crier frasnxex");
+$("#output").val('GONADS');
+console.log("TESTES");
+
+var search = $("input");
+var code = $("pre");
+
+
+$("#search").keyup(function() {
+  alert('you pressed a key motherfucker');
+});

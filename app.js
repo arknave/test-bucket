5bd7b20635f968e99efc465d1a5b879aa08f631a
alexfreed <freed.alex@gmail.com>
2012-11-14 14:46:54 -0600

    revert back to old schema

--- app.js
+++ app.js
@@ -21,32 +21,33 @@ var text, err;
 var tournamentSchema = new mongoose.Schema({
   name: String,
   year: Number,
-  difficulty: Number,
-  packet: {
-    tossup: {	
-      subject: String, 
-      idTournament: Number,
-      packetName: String, 
-      tossupNumber: Number,
-      tossupText: String,
-      tossupAnswer: String
-      },
-    bonus: {
-      subject: String, 
-      idTournament: Number,
-      packetName: String, 
-      bonusNumber: Number,
-      bonusPreamble: String,
-      bonusQuestion1: String, 
-      bonusAnswer1: String, 
-      bonusQuestion2: String, 
-      bonusAnswer2: String,
-      bonusQuestion3: String, 
-      bonusAnswer3: String
-      }
-   }
+  difficulty: Number
 })
 var tournament = db.model('tournament', tournamentSchema);
+
+var tossupSchema = new mongoose.Schema({
+  subject: String, 
+  idTournament: Number,
+  packetName: String, 
+  tossupNumber: Number,
+  tossupText: String,
+  tossupAnswer: String
+})
+
+var bonusSchema = new mongoose.Schema({
+  subject: String, 
+  idTournament: Number,
+  packetName: String, 
+  bonusNumber: Number,
+  bonusPreamble: String,
+  bonusQuestion1: String, 
+  bonusAnswer1: String, 
+  bonusQuestion2: String, 
+  bonusAnswer2: String,
+  bonusQuestion3: String, 
+  bonusAnswer3: String
+})
+var bonus = db.model('bonus',bonusSchema); 
 
 
 app.get('/', function(req, res){
@@ -54,12 +55,11 @@ app.get('/', function(req, res){
 });
 
 app.post('/upload', function(req,res){
-  var parser = require('./parser.js');
-  parser.convert(req.files.uploadfile.path, function(){
-    parser.parse(req.files.uploadfile.path+".txt", 'utf8');
-  });
+  console.log(req.body);
+  console.log(req.files);
   res.redirect('back');
 });
+
 
 app.get('/upload', function(req, res){
   res.render('upload');
@@ -71,16 +71,18 @@ app.get('/search/', function(req,res){
 
 app.get('/search/:query?', function(req,res) {
   var query = req.params.query;
+  return db.query;
 });
 
 app.get('/client.js', function(req,res){
   res.sendfile(__dirname + '/client.js');
 });
 
-app.get('/public/images/:image', function(req,res){
-  res.sendfile(__dirname + '/public/images/'+req.params.image);
+app.get('/upload', function(req,res) {
+  res.render('upload');
+  res.redirect('/search/');
 });
-
+ 
 var port = process.env.PORT || 5000;
 
 app.listen(port, function() {

fa5bb4b5e88a8a38566380996708880f728fa7f8
alexfreed <freed.alex@gmail.com>
2012-11-16 14:45:38 -0600

    rj

--- app.js
+++ app.js
@@ -55,21 +55,27 @@ app.get('/', function(req, res){
 });
 
 app.post('/upload', function(req,res){
-/*  var temp = new tournament({name: req.body.tname, year: req.body.tyear, difficulty: req.body.diff});
+ /*var temp = new tournament({name: req.body.tname, year: req.body.tyear, difficulty: req.body.diff}); 
   temp.save(function (err) {
     if (err) return handleError(err);
-    console.log(temp);
-  });
+    console.log(temp);*/
+  tournament.create({
+    name: req.body.tname, 
+    year: req.body.tyear,
+    difficutly: req.body.diff
+  }, function(err) {
+      if(err)return handleError(err);
+    }
+  );
 
-Person.findOne({ 'name' : 'ACF Fall' }, 'name', function (err, tournament) {
+/*Person.findOne({}, 'name', function (err, tournament) {
     if (err) return handleError(err);
     console.log('%s is a tournament.',tournament.name);
     )};
-*/
   console.log(req.body);
   console.log(req.files);
-  console.log(req.files.uploadfile.path);
-  parser.zipconv(req.files.uploadfile.path, function() {});
+  console.log(req.files.uploadfile.path); */
+  //parser.zipconv(req.files.uploadfile.path, function() {});
   res.redirect('back');
 });
 
@@ -101,3 +107,5 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+

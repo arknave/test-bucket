46727146ce96f358b0ebd14251e60d5f6c402edf
arnav <arn@serv>
2013-02-06 15:36:59 -0600

    ajax notification

--- public/client.js
+++ public/client.js
@@ -1,13 +1,23 @@
 var search = document.getElementById('searchbar');
 var code = document.getElementById('output');
 
+$(document).ajaxSend(function(e, jqXHR){
+  $("#load").text('loading');
+});
+$(document).ajaxComplete(function(e, jqXHR){
+  //remove the div here
+  $("#load").text('done loading');
+});
+
+
 search.addEventListener('keyup', function(){
-  var xhr = new XMLHttpRequest;
-  xhr.open('GET', 'http://173.172.90.120:9200/questions/_search?q=' + search.value, true);
-  xhr.onreadystatechange = function(){
-    if (4 == xhr.readyState) {
-      var ray = JSON.parse(xhr.responseText).hits.hits;
-      var a = '';
+  if(!search.value) { return; }
+  $.ajax({
+    url: 'http://173.172.90.120:9200/questions/_search',
+    data: {q: search.value}
+  }).done(function( data ){
+      var a = '',
+        ray = JSON.parse(data).hits.hits;
       ray.forEach(function(q){
         ques = q._source;
         a += ''+ques.tmt.name+' '+ques.tmt.year+' - '+ques.pack+'\n';
@@ -15,7 +25,6 @@ search.addEventListener('keyup', function(){
         switch(q._type){
           case "tossup":
             a += 'ANSWER: '+ques.ans;
-            break;
           case "bonus":
             a += '[10] '+ques.part1+'\n';
             a += 'ANSWER: '+ques.ans1+'\n';
@@ -28,7 +37,13 @@ search.addEventListener('keyup', function(){
         a += '\n';
       }); 
       code.textContent = a;
+  });
+/*  var xhr = new XMLHttpRequest;
+  xhr.open('GET', 'http://173.172.90.120:9200/questions/_search?q=' + search.value, true);
+  xhr.onreadystatechange = function(){
+    if (4 == xhr.readyState) {
     }
   };
   xhr.send();
-}, false);
+}, false);*/
+});
